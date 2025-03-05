508090367eadcee1f069f888c76aabe035ca72cc
AutoAFK <ranhaim85@gmail.com>
2025-03-05 11:45:02 +0200

    Seperated the main function into multiple functions for better readabilty

--- Program.cs
+++ Program.cs
@@ -1,55 +1,90 @@
-﻿namespace MathGame
+﻿using System;
+
+namespace MathGame
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            var game = new Game(Difficulty.Easy);
-            while (true)
-            {
-                Console.Clear();
-                DisplayMenu();
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var game = new Game(Difficulty.Easy);
+			HandleUserChoise(ref game);
+		}
 
-                Console.WriteLine();
-                Console.Write("Please choose a number:");
-                bool success = int.TryParse(Console.ReadLine(), out int userOption);
-                if (!success)
-                {
-                    Console.WriteLine("Please input only numbers.");
-                    continue;
-                }
-                switch (userOption)
-                {
-                    case 1:
-                        game.CreateQuestion();
-                        break;
-                    case 2:
-                        // TODO: Implement show result.
-                        break;
-                    case 3:
-                        // TODO: Implement change difficulty.
-                        break;
-                    case 0:
-                        Console.WriteLine("Goodbye!");
-                        return;
-                    default:
-                        Console.WriteLine("Please choose one of the numbers of the list.");
-                        break;
-                }
-                Console.ReadKey();
-            }
-        }
+		private static void DisplayMenu()
+		{
+			Console.WriteLine("Math Game");
+			Console.WriteLine("---------");
+			Console.WriteLine();
+			Console.WriteLine("1. Start Game");
+			Console.WriteLine("2. List Results");
+			Console.WriteLine("3. Set game difficulty");
+			Console.WriteLine("0. Exit");
 
-        private static void DisplayMenu()
-        {
-            Console.WriteLine("Math Game");
-            Console.WriteLine("---------");
-            Console.WriteLine();
-            Console.WriteLine("1. Start Game");
-            Console.WriteLine("2. List Results");
-            Console.WriteLine("3. Set game difficulty");
-            Console.WriteLine("0. Exit");
+		}
 
-        }
-    }
+		private static void HandleUserChoise(ref Game game)
+		{
+			while (true)
+			{
+				Console.Clear();
+				DisplayMenu();
+
+				Console.WriteLine();
+				int userOption = GetUserInputOption(3);
+				switch (userOption)
+				{
+					case 1:
+						int rounds = GetInt("Question amount");
+						for (var i = 0; i < rounds; i++)
+						{
+							game.CreateQuestion();
+						}
+						break;
+					case 2:
+						game.ShowResults();
+						break;
+					case 3:
+						game.SetProblemsDifficulty();
+						break;
+					case 0:
+						Console.WriteLine("Thank you for playing!");
+						return;
+				}
+			}
+		}
+
+		private static int GetUserInputOption(int maxOption, int minOption = 0)
+		{
+			do
+			{
+				var userInput = GetInt("Enter option");
+				if (userInput < minOption || userInput > maxOption)
+				{
+					Console.WriteLine("Please choose an option from the list above.");
+				}
+				else
+				{
+					return userInput;
+				}
+			} while (true);
+			
+		}
+
+		private static int GetInt(string msg)
+		{
+			do
+			{
+				Console.Write($"{msg}: ");
+				var success = int.TryParse(Console.ReadLine(), out int userInput);
+				if (!success)
+				{
+					Console.WriteLine("Please enter only numbers.");
+				}
+				else
+				{
+					return userInput;
+				}
+			} while (true);
+		}
+	}
 }
